4769488a0c03c4b173030843f8a0cc2dcaec0aaa
Elena Shestakova <ShestakovaElenaA@yandex.ru>
2023-09-11 18:16:59 +0300

    reformat

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,7 @@
 public class BmiService {
     public double Calculate(double height, double weight) {
         double index;
-        {
-            index = (weight / (height * height));
-        }
+        index = (weight / (height * height));
         return index;
     }
 }
